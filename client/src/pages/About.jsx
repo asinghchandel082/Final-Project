2787ff361a9aa33e7454768ede10ce6ddfa9dd29
Ajay Singh Chandel <asinghchandel082@gmail.com>
2024-03-29 17:03:11 +0530

    Complete Project

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
-        About Sahand Estate
+        About PropHub
       </h1>
       <p className="mb-4 text-slate-700">
       PropHub is your one-stop solution for all things real estate. 
